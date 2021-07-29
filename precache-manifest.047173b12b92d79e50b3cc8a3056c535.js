6d5c59f1498ead4e1d7cc767aa9010f236a5884d
Sviatoslav Kishka <sviatoslavkishka@gmail.com>
2021-07-29 11:06:18 +0300

    Updates

---
+++ precache-manifest.047173b12b92d79e50b3cc8a3056c535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85c208e70fb2003d436bc5e03a66988",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c39b4177b7f2887eefd1",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "d4d173d40aacb7d09906",
+    "url": "./static/js/2.5e9091a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5e9091a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39b4177b7f2887eefd1",
+    "url": "./static/js/main.fc028dfc.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
